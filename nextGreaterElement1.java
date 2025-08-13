77af21e8edc7dd6b8b88e97ee65939d006181f3f
Sambhav Roy <sambhav.roy26@gmail.com>
2025-08-13 22:00:08 +0530

    Create nextGreaterElement1.java

---
+++ nextGreaterElement1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer>s=new Stack<>();
+        int nxtGreater[]=new int[10001];
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek()<= nums2[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nxtGreater[nums2[i]]=-1;
+            }else{
+                nxtGreater[nums2[i]]=s.peek();
+            }
+            s.push(nums2[i]);
+        }
+
+        for(int i=0;i<nums1.length;i++){
+            nums1[i]= nxtGreater[nums1[i]];
+        }
+
+        return nums1;
+    }
+}
