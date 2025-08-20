e381883a83b3700522b6a5b7912271200237f3ab
Sambhav Roy <sambhav.roy26@gmail.com>
2025-08-20 23:54:18 +0530

    Create LemonadeChange.java

---
+++ LemonadeChange.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills){
+        int count_5=0;
+        int count_10=0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                count_5+=1;
+            }
+            else if (bill ==10) {
+                if (count_5 ==0) return false;
+                count_5 -=1;
+                count_10 +=1;
+            }
+            else if (bill ==20) {
+                if (count_10 >=1 && count_5 >=1) {
+                    count_10 -=1;
+                    count_5 -=1;
+                }
+                else if (count_5 >=3) {
+                    count_5 -=3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
