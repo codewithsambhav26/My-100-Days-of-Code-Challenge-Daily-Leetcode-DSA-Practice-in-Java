194322ae25fe7a4aeafbc50ab03db0753368ea1b
Sambhav Roy <sambhav.roy26@gmail.com>
2025-09-28 00:11:22 +0530

    Create RemoveLLelements.java

---
+++ RemoveLLelements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp=new ListNode(0);
+        ListNode curr=temp;
+        temp.next=head;
+        while(curr.next!=null ){
+            if(curr.next.val==val){
+                curr.next=curr.next.next;
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+        
+        return temp.next;
+    }
+}
